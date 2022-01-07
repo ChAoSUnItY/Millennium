f602d51df8cefdaaeafedb140aee570ac37ee53e
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2022-01-07 16:19:12 +0800

    Add Barrel Block

--- src/main/java/io/github/cleanroommc/millennium/common/blocks/BarrelBlock.java
+++ src/main/java/io/github/cleanroommc/millennium/common/blocks/BarrelBlock.java
@@ -2,15 +2,39 @@ package io.github.cleanroommc.millennium.common.blocks;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 
-public class BarrelBlock extends HorizontalBlock {
+public class BarrelBlock extends DirectionalBlock {
+  public static final PropertyBool OPEN = PropertyBool.create("open");
+
   public BarrelBlock() {
     super(
         new Settings(Material.WOOD)
             .strength(2.5F)
             .soundType(SoundType.WOOD)
             .translationKey("barrel"));
+    setDefaultState(getBlockState().getBaseState().withProperty(OPEN, false));
   }
 
+  @Override
+  public int getMetaFromState(IBlockState state) {
+    int meta = super.getMetaFromState(state);
 
+    if (state.getValue(OPEN)) meta |= 8;
+
+    return meta;
+  }
+
+  @Override
+  public IBlockState getStateFromMeta(int meta) {
+    return getDefaultState().withProperty(FACING, EnumFacing.byIndex(meta & 7));
+  }
+
+  @Override
+  protected BlockStateContainer createBlockState() {
+    return new BlockStateContainer(this, FACING, OPEN);
+  }
 }
